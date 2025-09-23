2ea21040e8407d9f566871aca3678a8373266751
Stefan Novaković <stefan.novakovic@vegaitglobal.com>
2025-09-23 11:20:44 +0200

    Basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,8 @@
 const Footer = () => {
-   return <footer className="h-6 text-white bg-black">Footer</footer>;
+   return (
+      <footer className="h-11 w-full max-w-[1920px] mx-auto px-4 py-2 text-center tracking-wider">
+         TT &copy; {new Date().getUTCFullYear()} All Rights Reserved
+      </footer>
+   );
 };
 export default Footer;
